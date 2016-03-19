shalture/shalture

nickserv/{setpass,sendpass_user}: more helpful message for logged-in users
Sometimes, people try SETPASS or SENDPASS when all they actually need is
SET PASSWORD, so point them there if applicable.

modules/nickserv/sendpass_user.c
@@ -125,7 +125,10 @@ wrong_syntax:
 
 	if (MOWGLI_LIST_LENGTH(&mu->logins) > 0)
 	{
-		command_fail(si, fault_noprivs, _("This operation cannot be performed on %s, because someone is logged in to it."), entity(mu)->name);
+		if (si->mu == mu)
+			command_fail(si, fault_already_authed, _("You are logged in and can change your password using the SET PASSWORD command."));
+		else
+			command_fail(si, fault_noprivs, _("This operation cannot be performed on %s, because someone is logged in to it."), entity(mu)->name);
 		return;
 	}
 

modules/nickserv/setpass.c
@@ -65,6 +65,12 @@ static void ns_cmd_setpass(sourceinfo_t *si, int parc, char *parv[])
 		return;
 	}
 
+	if (si->mu == mu)
+	{
+		command_fail(si, fault_already_authed, _("You are logged in and can change your password using the SET PASSWORD command."));
+		return;
+	}
+
 	if (strlen(password) >= PASSLEN)
 	{
 		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "SETPASS");

staticfox/atheme

modules/nickserv/sendpass_user.c
@@ -108,7 +108,10 @@ static void ns_cmd_sendpass(sourceinfo_t *si, int parc, char *parv[])
 
 	if (MOWGLI_LIST_LENGTH(&mu->logins) > 0)
 	{
-		command_fail(si, fault_noprivs, _("This operation cannot be performed on %s, because someone is logged in to it."), entity(mu)->name);
+		if (si->smu == mu)
+			command_fail(si, fault_already_authed, _("You are logged in and can change your password using the SET PASSWORD command."));
+		else
+			command_fail(si, fault_noprivs, _("This operation cannot be performed on %s, because someone is logged in to it."), entity(mu)->name);
 		return;
 	}
 

modules/nickserv/setpass.c
@@ -65,6 +65,12 @@ static void ns_cmd_setpass(sourceinfo_t *si, int parc, char *parv[])
 		return;
 	}
 
+	if (si->smu == mu)
+	{
+		command_fail(si, fault_already_authed, _("You are logged in and can change your password using the SET PASSWORD command."));
+		return;
+	}
+
 	if (strlen(password) >= PASSLEN)
 	{
 		command_fail(si, fault_badparams, STR_INVALID_PARAMS, "SETPASS");